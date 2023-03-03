51875f7b4b308b294403c12274d831e3576919bb
TobiWan8 <tobiaslarsen93@gmail.com>
2023-03-03 12:09:21 +0100

    text animation

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,23 @@ document.body.onpointermove = event => {
         top: `${clientY}px`
     }, {duration: 3000, fill: "forwards"});
 }
+
+const letters = "abcdefghijklmnopqrstuvwxyzæøå";
+document.querySelector("h1").onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if(index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+            return letters[Math.floor(Math.random() * 26)]
+            })
+        .join("");
+
+
+        if(iterations >= 9) clearInterval(interval)
+        iterations += 1 / 3;
+    }, 30);
+}
